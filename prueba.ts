f06ae98a028eb1bc030d562563cf80a56299d2ca
rdlassoe <robinsonlasso2021@itp.edu.co>
2025-04-05 00:12:31 -0500

    se creo error de tipado en prueba.ts

---
+++ prueba.ts
@@ -0,0 +1,12 @@
+// suma.ts
+
+// Función que suma dos números
+function sumar(a: number, b: number): number {
+    return a + b;
+  }
+  
+  // Probamos la función
+  const resultado = sumar(5, 7);
+  consolelog
+  (`El resultado de la suma es: ${resultado}`);
+  
